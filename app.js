7bcd77468d446b502414d6f609e1bfe85dc33d13
eowsley <66394238+eowsley@users.noreply.github.com>
2020-10-30 22:12:51 -0700

    final commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById('hello').addEventListener('click', () => {
+    document.write('hello') ;
+ });
+ document.getElementById('goodbye').addEventListener('click', () => {
+    document.write('goodbye') ;
+ });
